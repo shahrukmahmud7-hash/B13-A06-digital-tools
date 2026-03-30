76ad86f6693ffababe32fa1b4f8534c1b1f88294
shahrukmahmud7-hash <shahrukmahmud7@gmail.com>
2026-03-30 19:35:24 +0600

    navbar

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ShoopingCart from '../../assets/products/shopping-cart.png'
+
+const NavBar = () => {
+    return (
+      <div className=''>
+   <div className='  mb-20'>
+       <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+
+    <a className="text-[#4F39F6] text-3xl font-bold px-20">DigiTools</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li className='text-[16px] text-semibold text-[#101727]'
+      ><a>Products</a></li>
+      <li className='text-[16px] text-semibold text-[#101727]'
+      ><a>Features</a></li>
+      <li className='text-[16px] text-semibold text-[#101727]'
+      ><a>Pricing</a></li>
+      <li className='text-[16px] text-semibold text-[#101727]'
+      ><a>Testimonials</a></li>
+      <li className='text-[16px] text-semibold text-[#101727]'
+      ><a>FAQ</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end gap-2 px-20">
+    <img src={ShoopingCart} alt="" />
+    <p className='text-[16px] text-semibold text-[#101727]'
+    >Login</p>
+    <button className="btn  text-white bg-gradient-to-r 
+    from-[#4F39F6] to-[#7A6CFF] border-none rounded-full">
+      Get Started
+    </button>
+  </div>
+</div>
+</div>
+      </div>
+    );
+};
+
+export default NavBar; 
